79cccca30db99d2ca7be4c0b620f4a6801b3eedc
Algren Pauna <algren.pauna@gmail.com>
2021-10-28 18:36:52 +0100

    changed post schema

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -2,19 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-    user: { type: String, required: true},
+const postSchema = new Schema(
+  {
+    user: { type: String, required: true },
     email: String,
     profilePic: String,
     type: String,
     profession: String,
     description: String,
     budget: Number,
-    location: String
-}, {
+    location: String,
+    distance: Number,
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.model('Post', postSchema);
 
 module.exports = Post;
